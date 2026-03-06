8241ecad2c1f11d902f4350e9b254f949de1f60d
ItsAltus <blakehuhn@live.com>
2026-03-06 16:50:56 -0700

    encapsulate elevator class better and add start floor to visited floors

--- src/elevator/simulator.py
+++ src/elevator/simulator.py
@@ -1,8 +1,9 @@
-time_per_floor = 10
 class Elevator:
     """
     Simple elevator that travels between floors.
     """
+
+    TIME_PER_FLOOR = 10
 
     def __init__(self, start_floor: int = 0):
         self.current_floor = start_floor
@@ -17,12 +18,14 @@ class Elevator:
             A tuple containing the total time taken to visit all stops and a list of the floors visited in order.
         """
         total_time = 0
-        visited_floors = []
+        visited_floors = [self.current_floor]
 
         for stop in stops_list:
             if stop != self.current_floor:
-                total_time += abs(stop - self.current_floor) * time_per_floor
-                visited_floors.append(stop)
+                travel_time = abs(stop - self.current_floor) * self.TIME_PER_FLOOR
+                total_time += travel_time
+
                 self.current_floor = stop
+                visited_floors.append(stop)
         
         return total_time, visited_floors
